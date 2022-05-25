3afd19965c2289d118fb5b8bcb8bd370ea168b32
BlagovestPanayotov <83649155+BlagovestPanayotov@users.noreply.github.com>
2022-05-25 22:45:11 +0100

    Fixing 3.First steps in programming folder

---
+++ 1.JavaScript Basics/3.First steps in programing/concatenateData.js
@@ -0,0 +1,11 @@
+function concatenate(input){
+
+    let firstName = input[0];
+    let lastName = input[1];
+    let age = Number(input[2]);
+    let town = input[3];
+
+    console.log(`You are ${firstName} ${lastName}, a ${age}-years old person from ${town}.`);
+
+}
+concatenate(['Blagovest','Panayotov','31','Sofia']);
