59db27d8c6e9e29a3c9e8b9c5e200278c6c09c6c
BlagovestPanayotov <83649155+BlagovestPanayotov@users.noreply.github.com>
2022-05-26 10:20:28 +0100

    Revert "Completed Fundamentals 11.Arrays"
    This reverts commit 51e2b85a61282aad988c3ced024c02f7be694851.

--- 2.JavaScript Fundamentas/11.Arrays/02.DayOfWeek.js
+++
@@ -1,7 +0,0 @@
-function dayOfWeek() {
-    let weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-
-    console.log(weekDays.toString());
-}
-dayOfWeek();
-
