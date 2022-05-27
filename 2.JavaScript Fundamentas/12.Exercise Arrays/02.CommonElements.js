55db814b312aa46ac93cfc05db383c5d614512d3
BlagovestPanayotov <83649155+BlagovestPanayotov@users.noreply.github.com>
2022-05-27 10:41:50 +0100

    Add first four tasks in Fundamentals JavaScript Arrays

---
+++ 2.JavaScript Fundamentas/12.Exercise Arrays/02.CommonElements.js
@@ -0,0 +1,9 @@
+function commonElements(arr1, arr2) {
+    for (let element of arr1) {
+        if (arr2.includes(element)) {
+            console.log(element);
+        }
+    }
+}
+
+commonElements(['Hey', 'hello', 2, 4, 'Peter', 'e'], ['Petar', 10, 'hey', 4, 'hello', '2']);
