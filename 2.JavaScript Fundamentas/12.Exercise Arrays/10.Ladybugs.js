8b8d5964e2c62b9c7f926c8756cef6e64d98d60b
BlagovestPanayotov <83649155+BlagovestPanayotov@users.noreply.github.com>
2022-06-07 09:17:20 +0100

    Fix lines in 10.Ladybugs.js

--- 2.JavaScript Fundamentas/12.Exercise Arrays/10.Ladybugs.js
+++ 2.JavaScript Fundamentas/12.Exercise Arrays/10.Ladybugs.js
@@ -1,22 +1,27 @@
 function ladybugs(input) {
 
-    let field = [];
-    for (i = 0; i < input[0]; i++) { //creating the field
-        field.push(0);
-    }
-    let fillingIndex = input[1].split(" ");
-    for (let i = 0; i < fillingIndex.length; i++) { //filling the field
-        let index = Number(fillingIndex[i]);
+    let fieldSize = input.shift();
+    let field = new Array(fieldSize).fill(0); //create field
+
+
+    let fillingIndex = input.shift().split(" ");
+    for (let position of fillingIndex) { //filling the field
+        let index = position;
         if (index >= 0 && index < field.length) {
             field[index] = 1;
         }
     }
-    for (let i = 2; i < input.length; i++) { //moving ladybugs
-        let currentCommand = input[i].split(" ");
-        let movingLadybug = Number(currentCommand[0]); // Separate command from strings
+
+    for (let i = 0; i < input.length; i++) { //moving ladybugs
+
+        let currentCommand = input[i].split(" ");   // Separate command from strings
+
+        let movingLadybug = Number(currentCommand[0]); 
         let direction = currentCommand[1];
         let step = Number(currentCommand[2]);
-        if (movingLadybug >= 0 && movingLadybug < field.length && field[movingLadybug] === 1) { //checking in the ladybug is in the field
+
+        if (field[movingLadybug] === 1) { //checking in the ladybug is in the field
+            field[movingLadybug] = 0;
             if (step < 0) {                                 //Corecting the direction
                 step = Math.floor(Math.abs(step));
                 if (direction === 'right') {
@@ -25,9 +30,8 @@ function ladybugs(input) {
                     direction = 'right';
                 }
             }
-
+            
             if (direction === 'right') {
-                field[movingLadybug] = 0;
                 while (field[movingLadybug + step] === 1) {      //increasing the step if needed 
                     step += step;
                 }
@@ -35,7 +39,6 @@ function ladybugs(input) {
                     field[movingLadybug + step] = 1;
                 }
             } else {
-                field[movingLadybug] = 0;
                 while (field[movingLadybug - step] === 1) {       //increasing the step if needed
                     step -= step;
                 }
@@ -49,6 +52,6 @@ function ladybugs(input) {
 
 }
 
-ladybugs([10, '0 1 2 3',
-    '0 right -1',
-    '2 right 1']);
+ladybugs([5, '0 1 2 3',
+    '0 right 0',
+    '2 right 0']);
