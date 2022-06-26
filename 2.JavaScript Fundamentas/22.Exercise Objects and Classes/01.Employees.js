fb7e5d1582e40d3054367b2727bdc14c4d9c0d9d
BlagovestPanayotov <83649155+BlagovestPanayotov@users.noreply.github.com>
2022-06-26 20:37:53 +0100

    Complete first tow tasks of 18.Exercise Arrays Advanced

---
+++ 2.JavaScript Fundamentas/22.Exercise Objects and Classes/01.Employees.js
@@ -0,0 +1,32 @@
+function employees(input) {
+
+    class Employee {
+        constructor (name){
+            this.name = name;
+            this.personalNumber = name.length;
+        }
+    }
+
+    let employees = []
+
+    for(let person of input){
+       employees.push(new Employee(person));
+    }
+
+    for(let employee of employees){
+        console.log(`Name: ${employee.name} -- Personal Number: ${employee.personalNumber}`);
+    }
+
+}
+employees([
+    'Silas Butler',
+    'Adnaan Buckley',
+    'Juan Peterson',
+    'Brendan Villarreal'
+]);
+employees([
+    'Samuel Jackson',
+    'Will Smith',
+    'Bruce Willis',
+    'Tom Holland'
+]);
---
+++ 2.JavaScript Fundamentas/22.Exercise Objects and Classes/02.Towns.js
@@ -0,0 +1,32 @@
+function towns(input){
+
+    let townsInArr = [];
+    for(let town of input){
+        townsInArr.push(town.split(' | '));
+    }
+    
+    
+    let townsInObj = [];
+
+    for(let city of townsInArr){
+        let town = city[0];
+        let latitude = Number(city[1]);
+        let longitude = Number(city[2]);
+
+        townsInObj.push({
+            town: town,
+            latitude: latitude.toFixed(2),
+            longitude: longitude.toFixed(2),
+        });
+    }
+
+   for (const town of townsInObj) {
+    console.log(town);
+   }
+
+}
+
+
+towns(['Sofia | 42.696552 | 23.32601',
+'Beijing | 39.913818 | 116.363625']);
+towns(['Plovdiv | 136.45 | 812.575']);
