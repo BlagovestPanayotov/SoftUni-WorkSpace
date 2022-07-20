cc8678c661299759deedd9292793eb79876132b4
BlagovestPanayotov <83649155+BlagovestPanayotov@users.noreply.github.com>
2022-07-20 07:48:24 +0100

    Fixed 02.BunnyKill.js in 18.Exercise Arrays Advanced - More Exercise

--- 2.JavaScript Fundamentas/18.Exercise Arrays Advanced/02.BunnyKill.js
+++ 2.JavaScript Fundamentas/18.Exercise Arrays Advanced/02.BunnyKill.js
@@ -1,57 +1,63 @@
-// !!! in the logic there is an infinity loop. must be fixed!!!
-
-
-// // function bunnyKill(hanger) {
-// //     let bombPositions = hanger.pop().split(' ').map(x => x.split(','));   //taking the coordinates in the bombs
-// //     hanger = hanger.map(x => x.split(' '));
-// //     let dealtDemage = 0;
-// //     let killedBunnies = 0;
-
-// //     for (let bombPosition of bombPositions) {
-// //         bombing(hanger, bombPosition);
-// //     }
-// //     console.table(hanger);
-
-
-// //     function bombing(hanger, bombPosition) {
-// //         let x = bombPosition[0];    //coordinates in the hanger
-// //         let y = bombPosition[1];
-
-// //         if (hanger[x][y] !== 0) {
-// //             let bombPower = hanger[x][y];
-// //             for (let i = -1; i <= 1; i++) {
-// //                 if (i >= 0 && i < hanger[0].length) {
-// //                     for (let j = -1; j <= 1; j++) {
-// //                         if (j >= 0 && j < hanger[0].length) {
-// //                             let currentPositionPower = [i, j];
-// //                             if (hanger[i][j] > 0) {     //check if there is a bunny bomb
-// //                                 if (hanger[i][j] - bombPower <= 0) {
-// //                                     bombing(hanger, currentPositionPower);
-// //                                     hanger[i][j] = 0;
-// //                                 } else {
-// //                                     hanger[i][j] -= bombPower;
-// //                                 }
-// //                             }
-// //                         }
-// //                     }
-// //                 }
-// //             }
-// //         }
-
-// //     }
-// // }
-
-
-
-
-
-
-// bunnyKill(['10 10 10',
-//     '10 10 10',
-//     '10 10 10',
-//     '0,0']);
-// // bunnyKill(['5 10 15 20',
-// //     '10 10 10 10',
-// //     '10 15 10 10',
-// //     '10 10 10 10',
-// //     '2,2 0,1']);
+function bunnyKill(arr) {
+    let coordinatesOfTheBombBunniesAsArr = arr
+      .pop()
+      .split(" ")
+      .map((el) => el.split(",").map((x) => Number(x)));
+    let matrixOfHangars = [];
+  
+    for (let i = 0; i < arr.length; i++) {
+      let rowOfMatrixAsArr = arr[i].split(" ").map(Number);
+      matrixOfHangars.push(rowOfMatrixAsArr);
+    }
+  
+    let damage = 0;
+    let counterOfKills = 0;
+  
+    for (let i = 0; i < coordinatesOfTheBombBunniesAsArr.length; i++) {
+      let currBombBunnyRow = coordinatesOfTheBombBunniesAsArr[i][0];
+      let currBombBunnyCol = coordinatesOfTheBombBunniesAsArr[i][1];
+      let damageOfCurrExplosion =
+        matrixOfHangars[currBombBunnyRow][currBombBunnyCol];
+      let startRow = Math.max(0, currBombBunnyRow - 1);
+      let endRow = Math.min(currBombBunnyRow + 1, matrixOfHangars.length - 1);
+  
+      if (damageOfCurrExplosion <= 0) {
+        continue;
+      }
+  
+      for (let j = startRow; j <= endRow; j++) {
+        let startCol = Math.max(0, currBombBunnyCol - 1);
+        let endCol = Math.min(
+          currBombBunnyCol + 1,
+          matrixOfHangars[0].length - 1
+        );
+  
+        for (let k = startCol; k <= endCol; k++) {
+          matrixOfHangars[j][k] -= damageOfCurrExplosion;
+        }
+      }
+  
+      damage += damageOfCurrExplosion;
+      counterOfKills++;
+    }
+  
+    for (let i = 0; i < matrixOfHangars.length; i++) {
+      for (let j = 0; j < matrixOfHangars[0].length; j++) {
+        if (matrixOfHangars[i][j] > 0) {
+          damage += matrixOfHangars[i][j];
+          counterOfKills++;
+        }
+      }
+    }
+  
+    console.log(damage);
+    console.log(counterOfKills);
+  }
+  bunnyKill([
+    "5 10 15 20",
+    "10 10 10 10",
+    "10 15 10 10",
+    "10 10 10 10",
+    "2,2 0,1",
+  ]);
+  bunnyKill(["10 10 10", "10 10 10", "10 10 10", "0,0"]);
