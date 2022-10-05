4a00886cc624ca55892015090d6440dad7164f14
BlagovestPanayotov <83649155+BlagovestPanayotov@users.noreply.github.com>
2022-10-05 01:56:20 +0100

    Complete the last two tasks in 16.Exercise Unit Testing and Error Handling of JS ADVANCED

--- 3.JS Advanced/JS ADVANCED/16.Exercise Unit Testing and Error Handling/06.Dynamic-Validation/app.js
+++ 3.JS Advanced/JS ADVANCED/16.Exercise Unit Testing and Error Handling/06.Dynamic-Validation/app.js
@@ -1,3 +1,14 @@
 function validate() {
-    // TODO
+    const input = document.getElementById('email');
+    input.addEventListener('change', emailValidator);
+
+    function emailValidator(ev) {
+        const validPattern = /^[a-z]+\@[a-z]+\.[a-z]+$/;
+        const text = ev.target.value
+        if (!text.match(validPattern)) {
+            ev.target.className = 'error';
+        } else {
+            ev.target.className = '';
+        }
+    }
 }
--- 3.JS Advanced/JS ADVANCED/16.Exercise Unit Testing and Error Handling/07.Form-Validation/formValidation.js
+++ 3.JS Advanced/JS ADVANCED/16.Exercise Unit Testing and Error Handling/07.Form-Validation/formValidation.js
@@ -1,3 +1,85 @@
 function validate() {
-    // TODO
+    const userNamePattern = /^([a-zA-Z0-9]){3,20}$/;
+    const passwordPattern = /^([\w]){5,15}$/;
+    const emailPattern = /@(\w)*\./;
+
+    const companyDiv = document.getElementById('companyInfo');
+    const checkbox = document.getElementById('company');
+    const submitButton = document.getElementById('submit');
+    const form = document.getElementById('registerForm');
+
+    const userName = form.querySelector('#username');
+    const email = form.querySelector('#email');
+    const password = form.querySelector('#password');
+    const confirmPassword = form.querySelector('#confirm-password');
+    const companyNumber = form.querySelector('#companyNumber');
+
+    const validDiv = document.getElementById('valid');
+
+
+    submitButton.addEventListener('click', validateSubmitForm);
+    checkbox.addEventListener('change', showHideCompanyInput);
+
+    function validateSubmitForm(ev) {
+        ev.preventDefault();
+
+
+        let isValid = true;
+
+        if (!userName.value.match(userNamePattern)) {
+            isValid = false;
+            userName.style['border-color'] = 'red';
+        } else {
+            userName.style.border = 'none';
+        }
+        if (!email.value.match(emailPattern)) {
+            isValid = false;
+            email.style['border-color'] = 'red';
+        } else {
+            email.style['border'] = 'none';
+        }
+        if (password.value !== confirmPassword.value) {
+            isValid = false;
+            password.style['border-color'] = 'red';
+            confirmPassword.style['border-color'] = 'red';
+        } else {
+            if (!password.value.match(passwordPattern)) {
+                isValid = false;
+                password.style['border-color'] = 'red';
+            } else {
+                password.style['border'] = 'none';
+            }
+            if (!confirmPassword.value.match(passwordPattern)) {
+                isValid = false;
+                confirmPassword.style['border-color'] = 'red';
+            } else {
+                confirmPassword.style['border'] = 'none';
+            }
+        }
+        if (checkbox.checked) {
+            if (Number(companyNumber.value) < 1000 || Number(companyNumber.value) > 9999) {
+                isValid = false;
+                companyNumber.style['border-color'] = 'red';
+            } else {
+                companyNumber.style['border'] = 'none';
+            }
+        }
+
+
+        if (isValid) {
+            validDiv.style.display = 'block';
+        } else {
+            validDiv.style.display = 'none';
+        }
+
+    }
+
+    function showHideCompanyInput(ev) {
+        if (ev.target.checked) {
+            companyDiv.style.display = 'block';
+        } else {
+            companyDiv.style.display = 'none';
+        }
+    }
 }
+
