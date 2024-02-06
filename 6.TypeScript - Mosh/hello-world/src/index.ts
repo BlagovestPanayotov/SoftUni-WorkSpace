ce70b4422b6bd347081eff258c4fff5fcc412b93
bobo2 <bobosmile1990@yahoo.com>
2024-02-06 11:52:53 +0000

    function

--- 6.TypeScript - Mosh/hello-world/src/index.ts
+++ 6.TypeScript - Mosh/hello-world/src/index.ts
@@ -1,14 +1,8 @@
-const small = 1;
-const medium = 2;
-const large = 3;
-
-// PascalCace
-const enum Size {
-  Small = 1, //default is 0. If we set naother number in the firest element, we will have the next element in ascending order
-  // if we set it to a string, we must scpecify the value of each element
-  Medium,
-  Large,
+function calculateTax(income: number, taxYear: number = 2022): number {
+  if (taxYear < 2022) {
+    return income * 1.2;
+  }
+  return income * 1.3;
 }
 
-let mySize: Size = Size.Medium;
-console.log(mySize);
+calculateTax(10_000);
