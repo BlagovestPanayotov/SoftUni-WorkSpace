43c594c183575e0c63c6ee511eced6029a939a93
bobo2 <bobosmile1990@yahoo.com>
2024-02-06 12:01:53 +0000

    objects

--- 6.TypeScript - Mosh/hello-world/src/index.ts
+++ 6.TypeScript - Mosh/hello-world/src/index.ts
@@ -1,9 +1,12 @@
-function calculateTax(income: number, taxYear: number = 2022): number {
-  if (taxYear < 2022) {
-    return income * 1.2;
-  }
-  return income * 1.3;
-}
-
-calculateTax(10_000);
-calculateTax(10_000, 2023);
+let empluyee: {
+  readonly id: number;
+  name: string;
+  fax?: number;
+  retire: (date: Date) => void;
+} = {
+  id: 1,
+  name: "Bobo",
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
