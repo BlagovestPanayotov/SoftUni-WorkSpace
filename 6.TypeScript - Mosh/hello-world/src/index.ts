7b9b886561cdfc8332210db673d39f7f4c36fc16
bobo2 <bobosmile1990@yahoo.com>
2024-02-06 12:14:46 +0000

    type aliases

--- 6.TypeScript - Mosh/hello-world/src/index.ts
+++ 6.TypeScript - Mosh/hello-world/src/index.ts
@@ -1,9 +1,11 @@
-let empluyee: {
+type Employee = {
   readonly id: number;
   name: string;
   fax?: number;
   retire: (date: Date) => void;
-} = {
+};
+
+let empluyee: Employee = {
   id: 1,
   name: "Bobo",
   retire: (date: Date) => {
