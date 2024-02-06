fdd5c1208abaeaaf8adc2d0737a6c55d17ca9924
bobo2 <bobosmile1990@yahoo.com>
2024-02-06 17:13:40 +0000

    Generic Interface

--- 6.TypeScript - Mosh/hello-world/dist/index.js
+++ 6.TypeScript - Mosh/hello-world/dist/index.js
@@ -1,8 +1,12 @@
 "use strict";
-class ArrayUtils {
-    static wrapInArray(value) {
-        return [value];
-    }
+var _a, _b;
+function fetch(url) {
+    console.log(url);
+    return { data: null, error: null };
 }
-let string = ArrayUtils.wrapInArray("s");
+let result = fetch("url");
+(_a = result.data) === null || _a === void 0 ? void 0 : _a.username;
+let result2 = fetch("url");
+(_b = result2.data) === null || _b === void 0 ? void 0 : _b.title;
+console.log(result);
 //# sourceMappingURL=index.js.map
--- 6.TypeScript - Mosh/hello-world/src/index.ts
+++ 6.TypeScript - Mosh/hello-world/src/index.ts
@@ -1,27 +1,29 @@
-// function wrapInArray<T>(value: T) {
-//   return [value];
-// }
+// http://mywebsite.com/users
+// http://mywebsite.com/products
 
-// let number = wrapInArray(1);
-// let string = wrapInArray("s");
-
-//////////////
-
-// class ArrayUtils {
-//   wrapInArray<T>(value: T) {
-//     return [value];
-//   }
-// }
-
-// let util = new ArrayUtils();
-// let string = util.wrapInArray("s");
-
-///////////////
-
-class ArrayUtils {
-  static wrapInArray<T>(value: T) {
-    return [value];
-  }
+interface Result<T> {
+  data: T | null;
+  error: string | null;
 }
 
-let string = ArrayUtils.wrapInArray("s");
+function fetch<T>(url: string): Result<T> {
+  console.log(url);
+
+  return { data: null, error: null };
+}
+
+interface User {
+  username: string;
+}
+
+interface Product {
+  title: string;
+}
+
+let result = fetch<User>("url");
+result.data?.username;
+
+let result2 = fetch<Product>("url");
+result2.data?.title;
+
+console.log(result);
