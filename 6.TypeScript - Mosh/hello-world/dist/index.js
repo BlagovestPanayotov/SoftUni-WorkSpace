c36652b5d7c975ebe6dcf136176f98878467a2b5
bobo2 <bobosmile1990@yahoo.com>
2024-02-06 17:50:58 +0000

    Generic Constants

--- 6.TypeScript - Mosh/hello-world/dist/index.js
+++ 6.TypeScript - Mosh/hello-world/dist/index.js
@@ -1,12 +1,15 @@
 "use strict";
-var _a, _b;
-function fetch(url) {
-    console.log(url);
-    return { data: null, error: null };
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
 }
-let result = fetch("url");
-(_a = result.data) === null || _a === void 0 ? void 0 : _a.username;
-let result2 = fetch("url");
-(_b = result2.data) === null || _b === void 0 ? void 0 : _b.title;
-console.log(result);
+class Customer extends Person {
+}
+function echo(value) {
+    return value;
+}
+echo({ name: "Bobo", age: 33 });
+echo(new Person("p"));
+echo(new Customer("c"));
 //# sourceMappingURL=index.js.map
--- 6.TypeScript - Mosh/hello-world/src/index.ts
+++ 6.TypeScript - Mosh/hello-world/src/index.ts
@@ -1,29 +1,17 @@
-// http://mywebsite.com/users
-// http://mywebsite.com/products
+// interface Person{
+//   name: string
+// }
 
-interface Result<T> {
-  data: T | null;
-  error: string | null;
+class Person {
+  constructor(public name: string) {}
 }
 
-function fetch<T>(url: string): Result<T> {
-  console.log(url);
+class Customer extends Person {}
 
-  return { data: null, error: null };
+function echo<T extends Person>(value: T): T {
+  return value;
 }
 
-interface User {
-  username: string;
-}
-
-interface Product {
-  title: string;
-}
-
-let result = fetch<User>("url");
-result.data?.username;
-
-let result2 = fetch<Product>("url");
-result2.data?.title;
-
-console.log(result);
+echo({ name: "Bobo", age: 33 });
+echo(new Person("p"));
+echo(new Customer("c"));
