3bf29b61a1d134ee3934f2632a6b659a04faf565
bobo2 <bobosmile1990@yahoo.com>
2024-02-06 11:12:00 +0000

    set up for debuging

--- 6.TypeScript - Mosh/hello-world/dist/index.js
+++ 6.TypeScript - Mosh/hello-world/dist/index.js
@@ -1,2 +1,7 @@
 "use strict";
 let age = 20;
+if (age < 50) {
+    age += 10;
+    console.log(age);
+}
+//# sourceMappingURL=index.js.map
--- 6.TypeScript - Mosh/hello-world/src/index.ts
+++ 6.TypeScript - Mosh/hello-world/src/index.ts
@@ -1,1 +1,6 @@
 let age: number = 20; // tsc index.ts
+
+if (age < 50) {
+  age += 10;
+  console.log(age);
+}
