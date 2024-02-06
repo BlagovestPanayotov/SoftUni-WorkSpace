c73afa4457f205dc7b5a31a6618b130c516236fe
bobo2 <bobosmile1990@yahoo.com>
2024-02-06 14:45:07 +0000

    Refactor: Parameter properties

--- 6.TypeScript - Mosh/hello-world/src/index.ts
+++ 6.TypeScript - Mosh/hello-world/src/index.ts
@@ -1,14 +1,11 @@
 class Account {
-  readonly id: number;
-  owner: string;
-  private _balance: number;
   nickname?: string;
 
-  constructor(id: number, owner: string, balance: number) {
-    this.id = id;
-    this.owner = owner;
-    this._balance = balance;
-  }
+  constructor(
+    public readonly id: number,
+    public owner: string,
+    private _balance: number
+  ) {}
 
   deposit(amount: number): void {
     if (amount <= 0) {
