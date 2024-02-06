f1c389745711a17a7409cd953f07ba0c709049bf
bobo2 <bobosmile1990@yahoo.com>
2024-02-06 13:01:19 +0000

    Type Assertions

--- 6.TypeScript - Mosh/hello-world/src/index.ts
+++ 6.TypeScript - Mosh/hello-world/src/index.ts
@@ -1,6 +1,8 @@
-let speed: number | null = null;
+let phone1 = document.getElementById("phone") as HTMLInputElement;
+let phone2 = <HTMLInputElement>document.getElementById("phone");
 
-let ride = {
-  //Nullish  coalescing operator
-  speed: speed ?? 30,
-};
+// HTMLElement
+// HTMLInputElement
+
+phone1.value;
+phone2.value;
