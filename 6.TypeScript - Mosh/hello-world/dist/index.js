f37e3b9c04ff7d6cecedd4e96aca9a5f949effd7
bobo2 <bobosmile1990@yahoo.com>
2024-02-06 12:43:45 +0000

    Optional Chaining

--- 6.TypeScript - Mosh/hello-world/dist/index.js
+++ 6.TypeScript - Mosh/hello-world/dist/index.js
@@ -1,10 +1,8 @@
 "use strict";
-function kgToLbs(weight) {
-    if (typeof weight === "number") {
-        return weight * 2.2;
-    }
-    return parseInt(weight) * 2.2;
+var _a;
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
 }
-kgToLbs(10);
-kgToLbs("10kg");
+let customer = getCustomer(1);
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
 //# sourceMappingURL=index.js.map
--- 6.TypeScript - Mosh/hello-world/src/index.ts
+++ 6.TypeScript - Mosh/hello-world/src/index.ts
@@ -1,6 +1,18 @@
-function greet(name: string | null | undefined) {
-  if (name) console.log(name.toUpperCase());
-  else console.log("Hola!");
+type Customer = {
+  birthday?: Date;
+};
+
+function getCustomer(id: number): Customer | null | undefined {
+  return id === 0 ? null : { birthday: new Date() };
 }
 
-greet(null);
+let customer = getCustomer(1);
+// Optional property access operator
+console.log(customer?.birthday?.getFullYear());
+
+// Oprional element access operator
+// customer?.[0]
+
+// Optional call
+let lod: any = null;
+log?.("a");
