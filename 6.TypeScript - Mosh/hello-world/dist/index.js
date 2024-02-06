db78d7be661ec9d78eb6bf662e2f7623de07f215
bobo2 <bobosmile1990@yahoo.com>
2024-02-06 20:53:23 +0000

    Module Formats

--- 6.TypeScript - Mosh/hello-world/dist/index.js
+++ 6.TypeScript - Mosh/hello-world/dist/index.js
@@ -1,27 +1,6 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __param = (this && this.__param) || function (paramIndex, decorator) {
-    return function (target, key) { decorator(target, key, paramIndex); }
-};
-const watchedParameters = [];
-function Watch(target, methodName, parameterIndex) {
-    watchedParameters.push({
-        methodName,
-        parameterIndex,
-    });
-}
-class Vehicle {
-    move(speed) {
-        console.log(speed);
-    }
-}
-__decorate([
-    __param(0, Watch)
-], Vehicle.prototype, "move", null);
-console.log(watchedParameters);
+Object.defineProperty(exports, "__esModule", { value: true });
+const shapes_1 = require("./shapes");
+const circle = new shapes_1.Circle(1);
+console.log(circle.radius);
 //# sourceMappingURL=index.js.map
--- 6.TypeScript - Mosh/hello-world/src/index.ts
+++ 6.TypeScript - Mosh/hello-world/src/index.ts
@@ -1,1 +1,5 @@
 import { Circle } from "./shapes";
+
+const circle = new Circle(1);
+
+console.log(circle.radius);
