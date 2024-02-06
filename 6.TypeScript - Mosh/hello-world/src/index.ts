7eb4848ca7ded3d8e9a4170bf39e3f114fc8f913
bobo2 <bobosmile1990@yahoo.com>
2024-02-06 14:41:25 +0000

    Private access modifier

--- 6.TypeScript - Mosh/hello-world/src/index.ts
+++ 6.TypeScript - Mosh/hello-world/src/index.ts
@@ -1,24 +1,31 @@
 class Account {
   readonly id: number;
   owner: string;
-  balance: number;
+  private _balance: number;
   nickname?: string;
 
   constructor(id: number, owner: string, balance: number) {
     this.id = id;
     this.owner = owner;
-    this.balance = balance;
+    this._balance = balance;
   }
 
   deposit(amount: number): void {
     if (amount <= 0) {
       throw new Error("Invalid amount");
     }
-    this.balance += amount;
+    // Record a transaction
+    this._balance += amount;
   }
+  getBalance(): number {
+    return this._balance;
+  }
+
+  private calculateTax() {}
 }
 
 const account = new Account(1, "Bobo", 1000);
+console.log(account.getBalance());
 
 account.deposit(1000);
 console.log(account instanceof Account);
