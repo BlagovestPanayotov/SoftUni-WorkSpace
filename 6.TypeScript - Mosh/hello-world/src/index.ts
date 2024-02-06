65dc5a0a5203cc14515bdafe9db0d9c45eb1b847
bobo2 <bobosmile1990@yahoo.com>
2024-02-06 12:29:19 +0000

    Intersection Type

--- 6.TypeScript - Mosh/hello-world/src/index.ts
+++ 6.TypeScript - Mosh/hello-world/src/index.ts
@@ -1,11 +1,14 @@
-function kgToLbs(weight: number | string): number {
-  // Narrowing
+type Draggable = {
+  drag: () => void;
+};
 
-  if (typeof weight === "number") {
-    return weight * 2.2;
-  }
-  return parseInt(weight) * 2.2;
-}
+type Resizable = {
+  resize: () => void;
+};
 
-kgToLbs(10);
-kgToLbs("10kg");
+type UIWidget = Draggable & Resizable;
+
+let textBox: UIWidget = {
+  drag: () => {},
+  resize: () => {},
+};
