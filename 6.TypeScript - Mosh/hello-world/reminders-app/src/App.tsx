f0a6e18e8fe148ad3de64e075b27ad7b6e1be31a
bobo2 <bobosmile1990@yahoo.com>
2024-02-07 00:38:12 +0000

    Using the State Hook

--- 6.TypeScript - Mosh/hello-world/reminders-app/src/App.tsx
+++ 6.TypeScript - Mosh/hello-world/reminders-app/src/App.tsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import "./App.css";
 import ReminderList from "./components/ReminderList";
 import Reminder from "./models/reminder";
 
-const reminders: Reminder[] = [{ id: 1, title: "Reminder1" }];
-
 function App() {
+  const [reminders, setReminders] = useState<Reminder[]>([
+    { id: 1, title: "Reminder1" },
+  ]);
+
   return (
     <div className="App">
       <ReminderList items={reminders} />
