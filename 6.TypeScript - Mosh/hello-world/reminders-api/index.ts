be8c9e6e94663fad74e0997c6442eef3d1cf02fb
bobo2 <bobosmile1990@yahoo.com>
2024-02-07 01:27:35 +0000

    Create a Basic Route

--- 6.TypeScript - Mosh/hello-world/reminders-api/index.ts
+++ 6.TypeScript - Mosh/hello-world/reminders-api/index.ts
@@ -2,4 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
 app.listen(8000, () => console.log("Server is running on port 8000..."));
